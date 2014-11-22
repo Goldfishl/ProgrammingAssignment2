7ac90369795feaaaa0f83788db97e783c87b2566
Goldfishl <liumhan@gmail.com>
2014-11-22 22:22:31 +0800

    add some spaces between lines

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 ## The two functions realize the storing and calculating the inverse matrix, so that if the matrix
 ## are not changing, we could directly get its inverse rather than recompute it.
 
+
 ## makeCacheMatrix() creates a list, which stores the original matrix and its inverse matrix.  
 ## The four functions are used to set or get the value of the matrix and the inverse matrix.
 
@@ -13,11 +14,13 @@ makeCacheMatrix <- function(x = matrix()) {
         get <- function() x       #return the value of original matix
         setinverse <- function(inverse) m <<- inverse  # The first time we call cacheInverse() for a specific matrix, 
         #this function will be accessed and it will store the inverse of that matrix. 
+
         getinverse <- function() m  # reture the cached value to cacheInverse()
         list(set = set, get = get,             
              setinverse = setinverse,
              getinverse = getinverse)
 }
+
 
 ## cacheInverse() accesses the list created when makeCacheMatrix was called. If the inverse has not yet been calculatd, 
 ## cacheInverse will calculate it and store it in the list. Else it will return the value of the inverse matrix.
